57ecdca5029180280c79b41faefa1d993518ba91
Justin <justingoh.ge@gmail.com>
2025-06-02 10:58:37 +0800

    Update StudyTimer.jsx

--- StudyTimerScreen/StudyTimer.jsx
+++ StudyTimerScreen/StudyTimer.jsx
@@ -5,16 +5,6 @@ import { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { CircleSlider } from 'react-native-circle-slider'; // You may need a library for circular dials
 
-// Firebase config
-const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
-};
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
