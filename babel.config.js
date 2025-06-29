9f0fbe151228452e35cf58c46df8aae1e01f2549
Brielle Ang <brielleang@Brielles-MacBook-Air.local>
2025-06-30 03:06:11 +0800

    Fixing runtime error

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,16 @@
 module.exports = function(api) {
-    api.cache(true);
-    return {
-      presets: [
-        'babel-preset-expo'
-      ],
-      plugins: [
-        ['module:react-native-dotenv', {
-          moduleName: '@env',
-          path: '.env',
-          safe: true, // Ensures .env file is validated
-          allowUndefined: false // Prevents undefined variables
-        }]
-      ]
-    };
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['module:react-native-dotenv', {
+        moduleName: '@env',
+        path: '.env',
+        safe: true,
+        allowUndefined: false
+      }],
+      ['@babel/plugin-transform-runtime'] 
+    ],
   };
+};
+
