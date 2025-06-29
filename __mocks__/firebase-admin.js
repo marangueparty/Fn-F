4cfe5b90bd40afc961ec2aa0cb5415222d516501
Brielle Ang <brielleang@Brielles-MacBook-Air.local>
2025-06-29 20:00:03 +0800

    Incl test changes

--- __mocks__/firebase-admin.js
+++ __mocks__/firebase-admin.js
@@ -1,53 +1,27 @@
+const setMock = jest.fn();
+const getMock = jest.fn();
+
+const docMock = jest.fn(() => ({
+  set: setMock,
+  get: getMock,
+  collection: jest.fn(() => ({ doc: docMock, get: getMock })),
+}));
+
+const collectionMock = jest.fn(() => ({ doc: docMock, get: getMock }));
+
+const firestoreMock = jest.fn(() => ({
+  collection: collectionMock,
+  doc: docMock,
+}));
+
 const FieldValue = {
   increment: jest.fn((val) => val),
   serverTimestamp: jest.fn(() => new Date()),
 };
 
-const set = jest.fn(() => Promise.resolve());
-const get = jest.fn(() =>
-  Promise.resolve({
-    docs: [
-      { id: 'achv1', data: () => ({ name: 'Badge One', description: 'desc1' }) },
-      { id: 'achv2', data: () => ({ name: 'Badge Two', description: 'desc2' }) },
-    ],
-  })
-);
-
-const doc = jest.fn(() => ({
-  set,
-  get: jest.fn(() => Promise.resolve({ exists: true, data: () => ({}) })),
-  collection: jest.fn(() => ({
-    doc,
-    get: jest.fn(() => Promise.resolve({
-      docs: [
-        { id: 'achv1', data: () => ({ name: 'Badge One', description: 'desc1' }) },
-        { id: 'achv2', data: () => ({ name: 'Badge Two', description: 'desc2' }) },
-      ],
-    })),
-  })),
-}));
-
-const collection = jest.fn(() => ({
-  doc,
-  get: jest.fn(() => Promise.resolve({
-    docs: [
-      { id: 'achv1', data: () => ({ name: 'Badge One', description: 'desc1' }) },
-      { id: 'achv2', data: () => ({ name: 'Badge Two', description: 'desc2' }) },
-    ],
-  })),
-}));
-
-const firestore = jest.fn(() => ({
-  collection,
-  doc,
-}));
-
-const initializeApp = jest.fn();
+firestoreMock.FieldValue = FieldValue;
 
 module.exports = {
-  initializeApp,
-  firestore,
-  FieldValue: {
-    serverTimestamp: jest.fn(() => 'mocked-timestamp'),
-  },
+  firestore: firestoreMock,
 };
+
